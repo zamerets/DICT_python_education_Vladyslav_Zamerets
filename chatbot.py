4c47ab239426aefc75d087699e17fd8fb6d389c7
zamerets <v.i.zamerets@student.khai.edu>
2022-10-10 18:36:57 +0300

    ChatBot 3 stage

--- chatbot.py
+++ chatbot.py
@@ -2,4 +2,12 @@ print("My name is Anton")
 print("Mt birth year is 2022")
 user_name = input("Please, remind me your name: ")
 print("What a great name you have, " + user_name + "!")
+print("Let me guess your age :) ")
+print("Please, enter reminder of your age by 3, 5, 7: ")
+remainder3 = input()
+remainder5 = input()
+remainder7 = input()
+age = (int(remainder3) * 70 + int(remainder5) * 21 + int(remainder7) * 15) % 105
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+
 
