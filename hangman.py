f45505a114cc0e1bb309edcc5d73015b1d5f42af
Zamerets <zameretsvlad@gmail.com>
2022-10-24 12:46:54 +0300

    Stage 2

--- hangman.py
+++ hangman.py
@@ -1,2 +1,15 @@
-print("HANGMAN")
-print("The game will be available soon...22")
+print("Welcome to Hangman")
+
+def game_start():
+    global words
+    global user_guess
+    words = "java"
+    user_guess = input("Guess the word: ")
+
+def start_condition():
+    game_start()
+    if words == user_guess:
+        print("You win!")
+    else:
+        print("You lose!")
+start_condition()
