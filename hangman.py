09c8aca7b29b11004e58e3cd38583810eaff8c2e
Zamerets <zameretsvlad@gmail.com>
2022-10-24 16:03:41 +0300

    Stage 4

--- hangman.py
+++ hangman.py
@@ -6,13 +6,27 @@ def game_start():
     global rnd_word
     words = 'python', 'java', 'javascript', 'php', 'ruby', 'kotlin'
     rnd_word = random.randint(0, 5)
-    print(words[rnd_word])
-    user_guess = input("Guess the word: ")
 
-def start_condition():
-    game_start()
+game_start()
+
+def user_word():
+    global guessed_word_list
+    global user_guess
+    guessed_word_list = list(words[rnd_word])
+    i=-1
+    for j in guessed_word_list:
+        i += 1
+        if(i>=3):
+            guessed_word_list[i]="-"
+
+    user_guess = input("Guess the word " + "".join(map(str,guessed_word_list)) + ": ")
+user_word()
+
+
+def win_condition():
     if words[rnd_word] == user_guess:
         print("You win!")
     else:
         print("You lose!")
-    start_condition()
+
+win_condition()
