aec3e23d3db4ed2fc9a1f5d7b6801b93c01465d7
Zamerets <zameretsvlad@gmail.com>
2022-10-24 13:07:20 +0300

    Stage 3

--- hangman.py
+++ hangman.py
@@ -1,15 +1,18 @@
-print("Welcome to Hangman")
-
+import random
 def game_start():
+    print("Welcome to Hangman")
     global words
     global user_guess
-    words = "java"
+    global rnd_word
+    words = 'python', 'java', 'javascript', 'php', 'ruby', 'kotlin'
+    rnd_word = random.randint(0, 5)
+    print(words[rnd_word])
     user_guess = input("Guess the word: ")
 
 def start_condition():
     game_start()
-    if words == user_guess:
+    if words[rnd_word] == user_guess:
         print("You win!")
     else:
         print("You lose!")
-start_condition()
+    start_condition()
